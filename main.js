900a1ad7f24614aa10994fcb405b1c0bad83fb8c
Callum <84125856+callum-cheshire@users.noreply.github.com>
2022-11-03 15:10:29 +0000

    started task 2

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 /* 
-👉 Write your kata here!
+Write a function that:
+
+Takes in a string and returns the amount of vowels and consonants, in that order, in the form of an array.
+
+If the value passed is not a string, return null.
+
+Example:
+  input: Chris
+  output: [1, 4]
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+function vowelsAndConsonants(input) {
+    // Write your code here!
+}
--- main.test.js
+++ main.test.js
@@ -1,1 +1,18 @@
 //👉 Write your tests below here:
+/*
+- Function should return an array
+
+- Input of any data type other than string should result in null.
+
+- Input Jonathan should result in [3, 5]
+- Input Gregory should result in [2, 5]
+*/
+
+import main from './main'
+import {jest} from '@jest/globals';
+import { expect, test } from '@jest/globals'
+
+test("Check if input is a string", () => {
+    let testData = true
+    
+})
